7395136c76e8b0edf24af198a7aad33e1011723f
Pushkar2730 <guptapushkar2730@gmail.com>
2021-09-30 15:15:53 +0530

    Update hypo.js

--- hypo.js
+++ hypo.js
@@ -1,14 +1,15 @@
 const sides=document.querySelectorAll(".side-input");
 const HypoBtn=document.querySelector("#hypo-btn");
-const output=document.querySelector("#output");
+const output=document.querySelector("#out");
 
 HypoBtn.addEventListener('click',CalcHypo)
 
 
 function CalcHypo(){
 const SumSqr=CalcSumOfSqr(Number(sides[0].value),Number(sides[1].value));
-const lengthHypo=Math.sqrt(SumSqr);
-output.innerText="length of hypotenuse  "+lengthHypo;
+let lengthHypo=Math.sqrt(SumSqr);
+const len=lengthHypo.toFixed(2);
+output.innerText="length of hypotenuse  "+len;
 }
 
 function CalcSumOfSqr(a,b){
